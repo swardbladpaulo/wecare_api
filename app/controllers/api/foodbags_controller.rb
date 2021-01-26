78d58612bd9076f061668d1926cd7804cc7b7155
flis_ann <ann.flismark@gmail.com>
2021-01-26 16:54:32 +0100

    Building on test for foodbag

--- app/controllers/api/foodbags_controller.rb
+++ app/controllers/api/foodbags_controller.rb
@@ -1,11 +1,13 @@
 class Api::FoodbagsController < ApplicationController
 
-  def create
-    foodbag = Foodbag.create
-    if foodbag.persisted?
-      render json: {message: 'Your foodbag was successfully created!'}, status: 201
-    else
-      render json: { message: foodbag.errors.full_messages.to_sentence }, status: 422
-  end
-end
+    def create
+      foodbag = Foodbag.create
+      binding.pry
+      if foodbag.persisted?
+        render json: {message: 'Your foodbag was successfully created!'}, status: 201
+      else
+        render json: { message: foodbag.errors.full_messages.to_sentence }, status: 422
+        binding.pry
+      end
+    end
 end
--- spec/factories/foodbags.rb
+++ spec/factories/foodbags.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :foodbag do
-    
+    pickup_time { 1 }
+    status { 1 }
+    association :donor
   end
 end
