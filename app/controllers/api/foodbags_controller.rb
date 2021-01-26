02d2eceb6048b6ef3062a11f4ccc0dc81b8939be
flis_ann <ann.flismark@gmail.com>
2021-01-26 20:56:48 +0100

    adding private method permitting pickuptime and status Running test in postman shows that it is only possible to send of a create with these and correct values

--- app/controllers/api/foodbags_controller.rb
+++ app/controllers/api/foodbags_controller.rb
@@ -1,11 +1,16 @@
 class Api::FoodbagsController < ApplicationController
 
     def create
-      foodbag = Foodbag.create
+      foodbag = Foodbag.create(foodbag_params)
       if foodbag.persisted?
         render json: {message: 'Your foodbag was successfully created!'}, status: 201
       else
         render json: { message: foodbag.errors.full_messages.to_sentence }, status: 422
       end
+    end
+
+    def foodbag_params
+      params.require(:foodbag).permit(:pickuptime, :status)
+      
     end
 end
